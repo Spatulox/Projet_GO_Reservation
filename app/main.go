51f0f75b415fe74adeb6f017c0b2d5acd7929a9b
AlexandreDjazz <alexandrehelleux@hotmail.fr>
2024-04-12 16:56:55 +0200

    Update main.go update menu

--- app/main.go
+++ app/main.go
@@ -4,16 +4,41 @@ import (
 	"fmt"
 )
 
-var choix int
+var option int
 
 func main() {
-	menu()
+	for {
+		menu()
+		switch option {
+		case 1:
+			// listerSallesDisponibles()
+		case 2:
+			// creerReservation()
+		case 3:
+			// annulerReservation()
+		case 4:
+			// visualiserReservations()
+		case 5:
+			fmt.Println("Au revoir!")
+			return
+		}
 
+	}
 }
 
 func menu() {
-
-	fmt.Println("Bienvenue dans le Service de Réservation en Ligne\n-----------------------------------------------------\n")
-	fmt.Println("1. Lister les salles disponibles\n2. Créer une réservation\n3. Annuler une réservation\n4. Visualiser les réservations\n5. Quitter\nChoisissez une option :")
-	fmt.Scanln(&choix)
+	for {
+		fmt.Println("Bienvenue dans le Service de Réservation en Ligne\n-----------------------------------------------------\n")
+		fmt.Println("1. Lister les salles disponibles\n2. Créer une réservation\n3. Annuler une réservation\n4. Visualiser les réservations\n5. Quitter\nChoisissez une option :")
+		_, err := fmt.Scanln(&option)
+		if err != nil {
+			fmt.Println("Erreur de saisie. Veuillez saisir un numéro valide.")
+			continue
+		}
+		if option < 1 || option > 5 {
+			fmt.Println("Option invalide. Veuillez choisir une option entre 1 et 5.")
+			continue
+		}
+		break
+	}
 }
