0630a1b703fdf7d28dd8b4169ed2673b55f9a1ea
AlexandreDjazz <alexandrehelleux@hotmail.fr>
2024-04-12 16:30:45 +0200

    Update main.go add function menu

--- app/main.go
+++ app/main.go
@@ -1,1 +1,19 @@
-package Projet_GO_Reservation
+package main
+
+import (
+	"fmt"
+)
+
+var choix int
+
+func main() {
+	menu()
+
+}
+
+func menu() {
+
+	fmt.Println("Bienvenue dans le Service de Réservation en Ligne\n-----------------------------------------------------\n")
+	fmt.Println("1. Lister les salles disponibles\n2. Créer une réservation\n3. Annuler une réservation\n4. Visualiser les réservations\n5. Quitter\nChoisissez une option :")
+	fmt.Scanln(&choix)
+}
