a22702bf75d1d14c4acc7944c5bdd74b6b817778
Spatulox <M78stormtrooper@laposte.net>
2024-04-14 21:11:25 +0200

    move ModelReservation to a model folder

---
+++ pkg/models/ModeleReservation.go
@@ -0,0 +1,10 @@
+package lechauve
+
+type Reservation struct {
+	HoraireStart  string
+	HoraireEnd    string
+	IdEtat        int64
+	IdReservation int64
+	NomSalle      string
+	PlaceSalle    int64
+}
