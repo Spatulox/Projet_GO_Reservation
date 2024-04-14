e95dd639fb7d85eb18dbae9d867bdcd44c111469
Spatulox <M78stormtrooper@laposte.net>
2024-04-14 14:37:50 +0200

    move usefull functions for BDD from function.go to BDD.go

--- bdd/BDD.go
+++ bdd/BDD.go
@@ -6,6 +6,8 @@ import (
 	"errors"
 	_ "github.com/go-sql-driver/mysql"
 	"reflect"
+	"strconv"
+	"strings"
 )
 
 type Db struct {
@@ -363,4 +365,69 @@ func checkData(table string, column []string, values []string, condition *string
 	}
 
 	return true
+}
+
+//
+// ------------------------------------------------------------------------------------------------ //
+//
+
+func ArrayToString(arr []string, noQuotes ...bool) string {
+	if len(arr) == 0 {
+		return ""
+	}
+
+	var sb strings.Builder
+	for i, s := range arr {
+		//sb.WriteString(s)
+
+		_, err := strconv.Atoi(s)
+		if err != nil && noQuotes == nil {
+			// Cast to int failed
+			sb.WriteString(`'` + s + `'`)
+		} else {
+			// Cast to int ok
+			sb.WriteString(s)
+		}
+
+		if i < len(arr)-1 {
+			sb.WriteString(",")
+		}
+	}
+	return sb.String()
+}
+
+//
+// ------------------------------------------------------------------------------------------------ //
+//
+
+func ConcatColumnWithValues(columns []string, values []string) string {
+
+	if len(columns) == 0 || len(values) == 0 {
+		Log.Error("Plz columns and values string array must have at least one key each")
+		return ""
+	}
+
+	if len(columns) != len(values) {
+		Log.Error("Plz columns and values string array must have the same length")
+		return ""
+	}
+
+	var sb strings.Builder
+	for i, s := range values {
+		//sb.WriteString(s)
+
+		_, err := strconv.Atoi(s)
+		if err != nil {
+			// Cast to int failed
+			sb.WriteString(columns[i] + `='` + s + `'`)
+		} else {
+			// Cast to int ok
+			sb.WriteString(columns[i] + "=" + s)
+		}
+
+		if i < len(columns)-1 {
+			sb.WriteString(",")
+		}
+	}
+	return sb.String()
 }
