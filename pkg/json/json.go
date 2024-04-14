79d872c45096902a9ccfcd9054539a3fe88aaa41
Spatulox <M78stormtrooper@laposte.net>
2024-04-14 18:18:59 +0200

    update json.go, add JsonToData
    WIP : Need to save the data in the BDD

--- pkg/json/json.go
+++ pkg/json/json.go
@@ -7,11 +7,7 @@ import (
 	"os"
 )
 
-func Json(data []map[string]interface{}) bool {
-	/*data := []map[string]interface{}{
-		{"name": "John Doe", "age": 30, "email": "john.doe@example.com"},
-		{"name": "Jane Smith", "age": 25, "email": "jane.smith@example.com"},
-	}*/
+func DataToJson(data []map[string]interface{}) bool {
 
 	jsonData, err := json.MarshalIndent(data, "", "  ")
 	if err != nil {
@@ -30,4 +26,34 @@ func Json(data []map[string]interface{}) bool {
 	Log.Infos("Fichier JSON enregistré avec succès.")
 
 	return true
+}
+
+func JsonToData(path *string) []map[string]interface{} {
+
+	var err error
+	var jsonData []byte
+
+	if path == nil {
+		Log.Error("Vous devez spécifier un chemin pour le fichier à importer")
+		Log.Debug("En cours de création")
+		return nil
+	} else {
+		jsonData, err = os.ReadFile(*path)
+	}
+	// Lire le fichier JSON
+	if err != nil {
+		Log.Error("Erreur lors de la lecture du fichier:", err)
+		return nil
+	}
+
+	// Transformer le JSON en slice de maps
+	var data []map[string]interface{}
+	err = json.Unmarshal(jsonData, &data)
+	if err != nil {
+		Log.Error("Erreur lors de la conversion en Go:", err)
+		return nil
+	}
+
+	// Afficher les données
+	return data
 }
