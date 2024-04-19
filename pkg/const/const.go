d56a009ea322e19fbd14a82d2b5681dd2dc5e985
Spatulox <M78stormtrooper@laposte.net>
2024-04-19 23:31:37 +0200

    update const.go, add comments

--- pkg/const/const.go
+++ pkg/const/const.go
@@ -4,11 +4,13 @@ import (
 	. "Projet_GO_Reservation/pkg/log"
 )
 
+// Constante pour les strings
 const (
 	NullString = ""
 	NullInt    = 0
 )
 
+// Constante pour les tables SQL
 const (
 	ETAT         = "ETATS"
 	RESERVATIONS = "RESERVATIONS"
@@ -16,4 +18,5 @@ const (
 	SALLES       = "SALLES"
 )
 
+// Instanciation d'une variable Log pour éviter de la faire dans les autres fichiers.
 var Log LogHelper
