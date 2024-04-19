896abcb9a6ecc727a6f0868963c373a829b542a8
Spatulox <M78stormtrooper@laposte.net>
2024-04-20 00:19:22 +0200

    update webConst, add comments

--- pkg/web/webConst.go
+++ pkg/web/webConst.go
@@ -5,19 +5,23 @@ import (
 )
 
 const (
+	// Global things
 	PORT       = "8085"
 	RouteIndex = "/"
 
+	// Route for reservation
 	RouteIndexReservation  = "/reservation"
 	RouteListReservation   = "/reservation/list"
 	RouteCreateReservation = "/reservation/create"
 	RouteUpdateReservation = "/reservation/update"
 	RouteCancelReservation = "/reservation/cancel"
 
+	// Route for Rooms
+	RouteGetAllRoolAvailable = "/salle/getAllAvail"
+
+	// Route for JSON
 	RouteDownloadJson = "/download"
 	RouteExportJson   = "/reservation/export"
-
-	RouteGetAllRoolAvailable = "/salle/getAllAvail"
 )
 
 // var templates = template.Must(template.ParseGlob("pkg/web/html/*.html"))
